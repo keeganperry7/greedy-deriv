0b93e4f84a10e4ace94518495f176ce586781dd2
keeganperry7 <keegan.perry.kp@gmail.com>
2025-01-03 20:24:10 +0200

    Added spans

--- GreedyDeriv/Locations.lean
+++ GreedyDeriv/Locations.lean
@@ -12,3 +12,28 @@ def Loc.right (loc : Loc α) : List α := loc.2
 
 @[simp]
 def Loc.word (loc : Loc α) : List α := loc.left.reverse ++ loc.right
+
+@[simp]
+def Loc.reverse (loc : Loc α) : Loc α :=
+  match loc with
+  | ⟨u, v⟩ => ⟨v, u⟩
+
+abbrev Span (σ : Type u) := List σ × List σ × List σ
+
+@[simp]
+def Span.left (s : Span σ) : List σ := s.1
+
+@[simp]
+def Span.match (s : Span σ) : List σ := s.2.1
+
+@[simp]
+def Span.right (s : Span σ) : List σ := s.2.1
+
+@[simp]
+def Span.reverse (s : Span σ) : Span σ :=
+  match s with
+  | ⟨s, u, v⟩ => ⟨v, u.reverse, s⟩
+
+def merge_start_end (start_loc end_loc : Loc σ) : Span σ :=
+  match start_loc, end_loc with
+  | ⟨s, _⟩, ⟨u, v⟩ => ⟨s, u.reverse.drop s.length, v⟩
